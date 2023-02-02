8706f8dabcacbade044cf882d41d38b91e519b71
altmoy <amoy@cfl.rr.com>
2023-02-01 20:58:48 -0500

    Minutes Announcement
    Added announcement

--- AFTACWebSite/_Scripts/myScript.js
+++ AFTACWebSite/_Scripts/myScript.js
@@ -4178,7 +4178,7 @@ $(document).ready(function () {
         subclicked = true;
         $('.underSel').css('color', 'yellow');
         $('#showAgenda').css('color', 'lightgreen');
-        window.open('_PDFs/agenda.pdf', '');
+        window.open('_PDFs/agenda1.pdf', '');
     });
 
     $('#showElected').click(function () {
