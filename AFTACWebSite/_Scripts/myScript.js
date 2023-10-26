ff719381a2339de516fb3205bc9feb693700ebbf
altmoy <amoy@cfl.rr.com>
2023-10-26 08:18:30 -0400

    Updates
    Updated COE and Desrosier 20 year wall jpg.

--- AFTACWebSite/_Scripts/myScript.js
+++ AFTACWebSite/_Scripts/myScript.js
@@ -239,7 +239,7 @@ $(document).ready(function () {
     });
 
     $('#butCOE').click(function () {
-        window.open('_pdfs/Calendar_2_Oct_2023.pdf', '');
+        window.open('_pdfs/Calendar_24_Oct_2023.pdf', '');
     });
 
     $('#myCorona').click(function () {
